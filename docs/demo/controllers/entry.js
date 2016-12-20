fd9d62b359a9a1004785855b9d0226b97541e34c
Ben Burton <benjamin.joseph.burton@gmail.com>
2016-12-20 11:37:42 -0500

    Add useful demo content

--- docs/demo/controllers/entry.js
+++ docs/demo/controllers/entry.js
@@ -1,2 +1,4 @@
-exports['corespring-extended-text-entry-ng15'] = require('corespring-extended-text-entry-ng15-controller');
-exports['corespring-extended-text-entry-ng15'].version =  '../node_modules/corespring-extended-text-entry-ng15/controller';
+
+         exports['corespring-extended-text-entry-ng15'] = require('corespring-extended-text-entry-ng15-controller');
+         exports['corespring-extended-text-entry-ng15'].version = '../node_modules/corespring-extended-text-entry-ng15/controller';
+         
